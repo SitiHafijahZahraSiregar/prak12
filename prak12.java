39a4f59df1efc869f3a6f07afa575d408f94da45
SitiHafijahZahraSiregar <92381404+SitiHafijahZahraSiregar@users.noreply.github.com>
2022-11-16 10:30:05 +0700

    Create prak12.java

---
+++ prak12.java
@@ -0,0 +1,15 @@
+package prak12_ara;
+
+import java.util.ArrayList;
+
+public class Prak12_Ara {
+
+    public static void main(String[] args) {
+      ArrayList<Integer>nilai = new ArrayList<>();
+      nilai.add(70);
+      nilai.add(75);
+      nilai.add(80);
+        System.out.println(nilai);
+    }
+    
+}
